73415ddabe5a54d7f8c3e3b500955ba23740f5cf
Mina Shin <me@minashin.net>
2020-11-02 08:08:37 -0800

    Separate echo services from main file

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("/api/echo", echo)
+	http.HandleFunc("/api/echo", api.EchoHandlerFunc)
 	http.HandleFunc("/api/books", api.BookHandleFunc)
 	http.ListenAndServe(port(), nil)
 }
@@ -26,10 +26,4 @@ func port() string {
 		port = "8080"
 	}
 	return ":" + port
-}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Query()["message"][0]
-	w.Header().Add("Content-Type", "text/plain")
-	fmt.Fprint(w, message)
 }
